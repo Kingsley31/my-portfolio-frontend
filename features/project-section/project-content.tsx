8db791e86610f627845ce1c13a1a37bb58b26b39
Kingsley31 <kingsleyemeka31@gmail.com>
2025-08-16 19:11:15 +0100

    fixed projects mobile responsivenes

--- features/project-section/project-content.tsx
+++ features/project-section/project-content.tsx
@@ -1,7 +1,7 @@
 export function ProjectContent({children}: Readonly<{
   children: React.ReactNode;
 }>) {
-    return (<div className="flex flex-col gap-2 items-center h-full p-30 md:p-30">
+    return (<div className="flex flex-col gap-2 items-center h-full pt-20 md:pt-30">
         {children}
     </div>);
 }
